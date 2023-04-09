40a858cacc0b8e30024b150c6cecb0f634b3392c
John Cassady <cassady3@tdl.com>
2023-04-09 12:10:31 -0700

    Added streamtest.py and 20a.wav.  They produce sound.

---
+++ streamtest.py
@@ -0,0 +1,34 @@
+# Adapted From YouTube StackOverflow
+# https://stackoverflow.com/questions/31674416/python-realtime-audio-streaming-with-pyaudio-or-something-else
+# https://people.csail.mit.edu/hubert/pyaudio/#examples
+
+import pyaudio
+import wave
+import sys
+
+CHUNK = 1024
+
+if len(sys.argv) < 2:
+    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
+    sys.exit(-1)
+
+wf = wave.open(sys.argv[1], 'rb')
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+
+data = wf.readframes(CHUNK)
+
+while data != '':
+    stream.write(data)
+    data = wf.readframes(CHUNK)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
+
