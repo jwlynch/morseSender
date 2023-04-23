1a910c292573e9aed0d552ebce069093bfe2050b
Jim <jim@jam.sessionsnet.org>
2023-04-23 15:38:32 -0700

    move open_stream func from streamtest.py to beeps.py

--- beeps.py
+++ beeps.py
@@ -1,2 +1,18 @@
 import pyaudio
 import numpy as np
+
+def open_stream(py_audio, sample_rate):
+    # samples are 32 bit floats
+    sample_width = 4
+    sample_format = py_audio.get_format_from_width(sample_width)
+
+    # Stream should have one channel
+    channels = 1
+    stream = py_audio.open(
+                        format=sample_format,
+                        channels=channels,
+                        rate=sample_rate,
+                        output=True
+                          )
+
+    return stream
--- streamtest.py
+++ streamtest.py
@@ -44,22 +44,6 @@ print("after p = pyaudio.PyAudio()")
 
 print("now create stream")
 
-def open_stream(py_audio, sample_rate):
-    # samples are 32 bit floats
-    sample_width = 4
-    sample_format = py_audio.get_format_from_width(sample_width)
-
-    # Stream should have one channel
-    channels = 1
-    stream = py_audio.open(
-                        format=sample_format,
-                        channels=channels,
-                        rate=sample_rate,
-                        output=True
-                          )
-
-    return stream
-
 print("done creating stream")
 
 #data = wf.readframes(CHUNK)
