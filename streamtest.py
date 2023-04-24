479a01e99ab5a7ebd71ed2cdf624308209d2b1b3
Jim <jim@jam.sessionsnet.org>
2023-04-23 19:07:42 -0700

    correct import from beeps, to import tone_wavtbl (not tone)

--- streamtest.py
+++ streamtest.py
@@ -7,7 +7,7 @@ import pyaudio
 import sys
 import numpy as np
 
-from beeps import open_stream, close_stream, tone, mk_pyaudio, stop_pyaudio
+from beeps import open_stream, close_stream, tone_wavtbl, mk_pyaudio, stop_pyaudio
 
 def mk_sine_table():
     waveform = np.sin
