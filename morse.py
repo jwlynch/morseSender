a2004114122a772ce0b42c55f4f5cb78db9dc0eb
Jim <jim@jam.sessionsnet.org>
2023-04-29 03:56:55 -0700

    have all the playback funcs also accept a stream

--- morse.py
+++ morse.py
@@ -20,71 +20,71 @@ char_space_time = dit_time * 3
 word_space_time = dit_time * 7
 dah_time = char_space_time
 
-def dit():
+def dit(stream):
     tone(stream, sample_rate, dit_time, frequency)
 
-def dah():
+def dah(stream):
     tone(stream, sample_rate, dah_time, frequency)
 
-def word_space():
+def word_space(stream):
     tone(stream, sample_rate, word_space_time, 0)
 
-def char_space():
+def char_space(stream):
     tone(stream, sample_rate, char_space_time, 0)
 
-def el_space():
+def el_space(stream):
     tone(stream, sample_rate, dit_time, 0)
 
-def dit_or_dah(s):
+def dit_or_dah(stream, s):
     if s == '.':
-        dit()
+        dit(stream)
     elif s == '-':
-        dah()
+        dah(stream)
 
-def play_ditdahs(s):
+def play_ditdahs(stream, s):
     if len(s) == 0:
         return
     elif len(s) == 1:
-        dit_or_dah(s)
+        dit_or_dah(stream, s)
     else: # more than one
         first = s[0]
         rest = s[1:]
 
-        dit_or_dah(first)
+        dit_or_dah(stream, first)
 
         for c in rest:
-            el_space()
-            dit_or_dah(c)
+            el_space(stream)
+            dit_or_dah(stream, c)
 
-def play_word(w):
+def play_word(stream, w):
     if len(w) == 0:
         return
-    elif len(w) == 1:
-        play_ditdahs(morse_dict[w])
+    elif len(stream, w) == 1:
+        play_ditdahs(stream, morse_dict[w])
     elif len(w) > 1:
         first = w[0]
         rest = w[1:]
 
-        play_ditdahs(morse_dict[first])
+        play_ditdahs(stream, morse_dict[first])
 
         for c in rest:
             char_space()
 
-            play_ditdahs(morse_dict[c])
+            play_ditdahs(stream, morse_dict[c])
 
-def play_string(s):
+def play_string(stream, s):
     words = s.split()
 
     if len(words) == 0:
         return
     elif len(words) == 1:
-        play_word(words[0])
+        play_word(stream, words[0])
     else:
         first = words[0]
         rest = words[1:]
 
-        play_word(first)
+        play_word(stream, first)
 
         for w in rest:
-            word_space()
-            play_word(w)
+            word_space(stream)
+            play_word(stream, w)
