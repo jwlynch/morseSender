96ec47571a7b44211ac3efc3624c7d30bce661ff
John Cassady <cassady3@tdl.com>
2023-04-30 05:24:58 -0700

    Fixed Submit botton callback to 'play_CQ'

--- mcGUI.py
+++ mcGUI.py
@@ -9,7 +9,8 @@ root = Tk()
 root.title('mcSoundOutput')
 root.geometry('600x400')
 
-# mp.play_string('CQ CQ CQ')
+def play_CQ():
+    mp.play_string('CQ CQ CQ')
 
 mcLabel = Label(root, text="Enter text to xmit in caps 50 chars max:")
 mcLabel.pack()
@@ -17,7 +18,7 @@ mcLabel.pack()
 mcTextbox = Entry(root, width=50)
 mcTextbox.pack()
 
-mcButton = Button(root, text='Submit', command=mp.play_string('CQ CQ'))
+mcButton = Button(root, text='Submit', command=play_CQ)
 mcButton.pack()
 
 root.mainloop()
