3be4fc7ad9c80be481a99c491763a5381226665d
Jim <jim@jam.sessionsnet.org>
2023-05-06 10:54:52 -0700

    add function to play in morse, what is in the text box

--- mcGUI.py
+++ mcGUI.py
@@ -16,6 +16,11 @@ mcTextbox = Entry(root, width=50)
 mcTextbox.pack()
 
 mcButton = Button(root, text='Submit', command=mp.play_string('CQ CQ'))
+def play_textbox():
+    text = mcTextbox.get()
+    text = text.upper()
+    mp.play_string(text)
+
 mcButton.pack()
 
 root.mainloop()
