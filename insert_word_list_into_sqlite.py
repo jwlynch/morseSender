e7e4a801f0a4045dc77921710c41b3c288236ee0
Jim <jim@jam.sessionsnet.org>
2023-05-30 10:34:42 -0700

    prog that will insert word list into sqlite db

---
+++ insert_word_list_into_sqlite.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+import os, re
+
+def mk_word_list():
+
+    word_list_pathlist = [os.path.sep, "usr", "share", "dict", "american-english"]
+
+    word_list_filename = os.path.join(*word_list_pathlist)
+
+    word_list = []
+
+    with open(word_list_filename, 'r') as word_list_h:
+        for w in word_list_h:
+            a_word = w.strip()
+
+            if re.match('^[A-Za-z]+$', a_word) and len(a_word) == 5:
+                word_list.append(a_word)
+
+    return word_list
+
+if __name__ == '__main__':
+    word_list = mk_word_list()
+
+    print(repr(word_list[:125]))
