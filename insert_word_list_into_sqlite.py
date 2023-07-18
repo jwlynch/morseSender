fe9cd6abc8f22c3056bd4ed1d7c4272c345fe505
Jim <jim@jam.sessionsnet.org>
2023-07-18 14:07:41 -0700

    alter func mk_word_list to add words to db instead of to list

--- insert_word_list_into_sqlite.py
+++ insert_word_list_into_sqlite.py
@@ -2,31 +2,42 @@
 
 import os, re, sqlite3
 
-def mk_word_list():
 def opendb():
     word_list_db_pathlist = [os.path.expanduser('~'), 'sqlite-dbs', 'amer_engl_wordlist.db']
     word_list_db_filename = os.path.join(*word_list_db_pathlist)
 
-    # create empty sqlite database
     conn = sqlite3.connect(word_list_db_filename)
 
-    # add table to the database, with col for word, and col (primary key) for index
     return conn
 
-    word_list_pathlist = [os.path.sep, "usr", "share", "dict", "american-english"]
 
-    word_list_filename = os.path.join(*word_list_pathlist)
 
-    word_list = []
+def mk_word_list(conn, c):
+
+    # add table to the database, with col for word, and col (primary key) for index
+    c.execute('''
+        create table words (
+            word text,
+            idex integer primary key
+        )
+    ''')
+
+    word_list_index = 0
 
     with open(word_list_filename, 'r') as word_list_h:
         for w in word_list_h:
             a_word = w.strip()
 
+            # if the word contains only letters, word is good to go into the db
             if re.match('^[A-Za-z]+$', a_word):
-                word_list.append(a_word)
+                # insert word (and index) into database
+                c.execute(f"insert into words values ('{a_word}', {word_list_index})")
 
-    return word_list
+                # increment index
+                word_list_index += 1
+
+                conn.commit()
+
 
 if __name__ == '__main__':
     pass
